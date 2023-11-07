58cbd15e632dc703872bb71d48fda07e8b1a3db4
Samil Abud <samilabud@gmail.com>
2023-11-07 15:43:10 -0400

    Fixed nextjs params

--- client/next.config.js
+++ client/next.config.js
@@ -1,18 +1,38 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    BACKEND_URL: "http://localhost:3080",
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3080",
-        pathname: "/images/articles/**",
-      },
-    ],
-  },
-};
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-module.exports = nextConfig;
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        BACKEND_URL: "http://localhost:3080",
+      },
+      images: {
+        remotePatterns: [
+          {
+            protocol: "http",
+            hostname: "localhost",
+            port: "3080",
+            pathname: "/images/articles/**",
+          },
+        ],
+      },
+    };
+  }
+
+  return {
+    env: {
+      BACKEND_URL: "https://lactaconsejosws.onrender.com/",
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "https://lactaconsejosws.onrender.com/",
+          port: process.env.PORT,
+          pathname: "/images/articles/**",
+        },
+      ],
+    },
+  };
+};
