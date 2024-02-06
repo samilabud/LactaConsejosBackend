3a67aae2a369197dcb9a457691b3cbfc7587de1c
Samil Abud <samilabud@gmail.com>
2024-02-05 23:06:03 -0400

    Added auth, middleware, validation and functions to autorize and authenticate the user

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useFormState, useFormStatus } from "react-dom";
+import { authenticate } from "../lib/actions";
+
+const LoginPage = () => {
+  //   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+
+  return (
+    <div>
+      <span>Login Form</span>
+    </div>
+  );
+};
+
+export default LoginPage;
