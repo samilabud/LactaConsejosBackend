5af00ae1cc2315f0c71192f383df2847fb9230bd
Samil Abud <samilabud@gmail.com>
2025-10-13 16:11:58 -0400

    removes unneeded console logs

--- client/src/lib/auth.ts
+++ client/src/lib/auth.ts
@@ -38,7 +38,7 @@ export const authOptions: NextAuthOptions = {
   ],
   callbacks: {
     session: ({ session, token }) => {
-      console.log("Session Callback", { session, token });
+      // console.log("Session Callback", { session, token });
       return {
         ...session,
         user: {
@@ -48,7 +48,7 @@ export const authOptions: NextAuthOptions = {
       };
     },
     jwt: ({ token, user }) => {
-      console.log("JWT Callback", { token, user });
+      // console.log("JWT Callback", { token, user });
       if (user) {
         const u = user as unknown as any;
         return {
